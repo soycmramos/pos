87938797b83d792b9c12c0a099f27963c1cc34a7
Carlos Ramos <carlosmarioramos34@gmail.com>
2024-09-22 17:28:02 -0500

    Add Update Product module

--- src/routes/products/index.js
+++ src/routes/products/index.js
@@ -2,6 +2,7 @@ import { randomUUID } from 'node:crypto'
 import { StatusCodes, ReasonPhrases } from 'http-status-codes'
 import { Router } from 'express'
 import Product from '../../database/models/Product.js'
+import { Sequelize } from 'sequelize'
 
 const router = Router()
 
@@ -152,6 +153,94 @@ router.get('/products/:idProduct', async (req, res) => {
 			code: StatusCodes.OK,
 			data: result
 		})
+})
+
+router.patch('/products/:idProduct', async (req, res) => {
+	const { idProduct } = req.params
+	const { code, name, description, price } = req.body
+	const requiredFields = { code, name, price }
+	const errors = []
+
+	for (const prop in requiredFields) {
+		if (requiredFields[prop] === 0) {
+			const code = 400
+			const detail = `${prop} cannot be 0`
+			errors.push({ code, detail })
+			continue
+		}
+
+		if (requiredFields[prop] && !String(requiredFields[prop]).trim().length) {
+			const code = 400
+			const detail = `${prop} cannot be empty`
+			errors.push({ code, detail })
+		}
+	}
+
+	if (errors.length) {
+		return res
+			.status(StatusCodes.BAD_REQUEST)
+			.json({
+				status: 'failure',
+				title: ReasonPhrases.BAD_REQUEST,
+				code: StatusCodes.BAD_REQUEST,
+				errors
+			})
+	}
+
+	let result = await Product.findOne({ where: { _id: idProduct } })
+
+	if (!result) {
+		const code = 404
+		const detail = `Product not found`
+		errors.push({ code, detail })
+	}
+
+	if (errors.length) {
+		return res
+			.status(StatusCodes.NOT_FOUND)
+			.json({
+				status: 'failure',
+				title: ReasonPhrases.NOT_FOUND,
+				code: StatusCodes.NOT_FOUND,
+				errors
+			})
+	}
+
+	try {
+		await Product.update({
+			code: Sequelize.fn('IFNULL', code, Sequelize.col('code')),
+			name: Sequelize.fn('IFNULL', name, Sequelize.col('name')),
+			description: Sequelize.fn('IFNULL', description, Sequelize.col('description')),
+			price: Sequelize.fn('IFNULL', price, Sequelize.col('price'))
+		}, { where: { _id: idProduct } })
+
+		result = await Product.findOne({ where: { _id: idProduct } })
+
+		return res
+			.status(StatusCodes.OK)
+			.json({
+				status: 'success',
+				title: ReasonPhrases.OK,
+				code: StatusCodes.OK,
+				data: result
+			})
+
+	} catch (error) {
+		console.error(error)
+		return res
+			.status(StatusCodes.INTERNAL_SERVER_ERROR)
+			.json({
+				status: 'failure',
+				title: ReasonPhrases.INTERNAL_SERVER_ERROR,
+				code: StatusCodes.INTERNAL_SERVER_ERROR,
+				errros: [
+					{
+						code: StatusCodes.INTERNAL_SERVER_ERROR,
+						detail: 'Something went wrong'
+					}
+				]
+			})
+	}
 })
 
 export default router
